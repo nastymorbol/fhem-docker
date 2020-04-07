c55f137e7af2dc1de6bee031fd92f72c4804ca7a
Schulze, Steve <S.Schulze@deos-ag.com>
2020-04-07 03:33:50 +0200

    Build trigger

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t nastymorbol/fhem:alpine .
+#docker build -t nastymorbol/fhem:alpine .
 #docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t nastymorbol/fhem:alpine --push .
 #docker run -it --rm -v "$(pwd)/fhem-dev/:/opt/fhem/" -p 8083:8083 --entrypoint /bin/sh --name fhem_alpine nastymorbol/fhem:alpine
 #nano Dockerfile
